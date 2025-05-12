50d0909700005578dbd9de54632fd8c48e091005
Basel Younis <bessoyounis@gmail.com>
2025-05-12 22:10:39 +0300

    login page checkpoint

--- src/GeneralComponents/TextField.tsx
+++ src/GeneralComponents/TextField.tsx
@@ -3,25 +3,62 @@ interface TextFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
   sizeFactor?: number;
   widthFactor?: number;
   heightFactor?: number;
+  fontFactor?: number;
   label?: string;
   placeholder?: string;
-  
+  className?: string;
+  value: string;
+  setValue: (value: string) => void;
 }
 
-const TextField: FunctionComponent<TextFieldProps> = (
-  {sizeFactor = 1,
+const TextField: FunctionComponent<TextFieldProps> = ({
+  sizeFactor = 1,
   widthFactor = 1,
-  heightFactor = 1}
-) => {
-    const baseStyle = {
-        width: `${widthFactor}rem`,
-        height: `${heightFactor}rem`,
-        fontSize: `${sizeFactor}rem`,
-        padding: "0.5rem",
-        borderRadius: "0.5rem",
-        border: "1px solid #ccc",
-    };
-  return <div style={baseStyle}>Text Field</div>;
+  heightFactor = 1,
+  fontFactor = 1,
+  ...props
+}) => {
+  const baseStyle = {
+    width: `${widthFactor * sizeFactor * 20}rem`,
+    height: `${heightFactor * sizeFactor * 3}rem`,
+    fontSize: `${fontFactor*sizeFactor}rem`,
+
+    borderRadius: "0.5rem",
+    border: "1px solid #ccc",
+    ...props.style,
+  };
+  return (
+    <div className={props.className}>
+      {props.label && (
+        <div style={{ fontFamily: "Poppins" ,fontSize: `${sizeFactor*fontFactor*0.9}rem` }} className="text-primary-light">
+          {props.label}
+        </div>
+      )}
+      <div
+        style={baseStyle}
+        className="flex flex-col p-2 items-center  justify-center mt-1"
+      >
+        <input
+          {...props}
+          value={props.value}
+          onChange={(e) => {
+            props.onChange?.(e);
+            props.setValue(e.target.value);
+          }}
+          
+          placeholder={props.placeholder || "Enter text here..."}
+          style={{
+            width: "100%",
+            height: "100%",
+            border: "none",
+            outline: "none",
+            fontSize: `${sizeFactor*fontFactor}rem`,
+          }}
+          className="bg-white"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default TextField;
--- src/pages/LoginPage/Components/LoginCard.tsx
+++ src/pages/LoginPage/Components/LoginCard.tsx
@@ -1,14 +1,20 @@
-import type { CSSProperties } from "@mui/material";
+import { type CSSProperties } from "@mui/material";
+import TextField from "../../../GeneralComponents/TextField";
+import { useState } from "react";
+import Button from "../../../GeneralComponents/Button";
+import { theme } from "../../../Constants/Colors";
 
 function LoginCard() {
   const baseStyle: CSSProperties = {
     boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.1)",
     aspectRatio: "553/832",
   };
+  const [email, setEmail] = useState<string>("");
+
   return (
     <div className="flex flex-col w-full h-fit justify-center items-center mt-30">
       <div
-        className="w-110 bg-white rounded-[10px] border-1 border-gray-200 p-6"
+        className="flex flex-col w-110 bg-white rounded-[10px] border-1 border-gray-200 p-6 px-8"
         style={baseStyle}
       >
         <div style={{ fontFamily: "Poppins" }} className="text-2xl">
@@ -17,6 +23,30 @@ function LoginCard() {
         <div style={{ fontFamily: "Poppins" }} className="text-gray-500">
           Log in to access your XPerdiem account
         </div>
+        <div className="flex flex-col mt-10 gap-y-5 ">
+          <TextField
+            label="Email Address"
+            style={{ width: "100%" }}
+            value={email}
+            setValue={setEmail}
+            placeholder="your@exmaple.com"
+          />
+          <TextField
+            label="Password"
+            type="password"
+            style={{ width: "100%" }}
+            value={email}
+            setValue={setEmail}
+            placeholder="Enter your password"
+          />
+        </div>
+        <Button
+          backgroundColor={theme.colors.primaryLight()}
+          textColor="white"
+          label="Log In"
+          style={{ alignSelf: "center", width: "100%",marginTop: "20px", fontSize: "1rem" }}
+          onHoverColor={theme.colors.primaryDark()}
+        />
       </div>
     </div>
   );
